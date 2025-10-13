d0a98fb66af7c02c4e1581eb84d2c8bc8181e636
haqsyistalanwar <hhaqsyis@gmail.com>
2025-10-13 10:13:04 +0700

    belajar-java

---
+++ JAVA/coba1.java
@@ -0,0 +1,12 @@
+class coba1 {
+public static void main(String[] args) {
+String nim = "I.2510249";
+String nama = "muhammad abdul haqsyist al anwar";
+String umur = "18";
+String profesi = "Dosen";
+System.out.println(nim);
+System.out.println(nama);
+System.out.println(umur);
+System.out.println(profesi);
+}
+}
