419873a7bfe8ddd81fc04d2987a7428581aeaf76
haqsyistalanwar <hhaqsyis@gmail.com>
2025-10-13 10:51:41 +0700

    latihan1

---
+++ C++/latihan1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ string nama="haqsyist";
+ int umur= 18;
+ float tinggi= 170;
+ bool aktif = true;
+
+ cout<<"nama:"<< nama <<endl;
+ cout<<"umur:"<< umur <<endl;
+ cout<<"tinggi:"<< tinggi <<endl;
+ cout<<"status aktif:"<< aktif << endl;
+ return 0;
+}
