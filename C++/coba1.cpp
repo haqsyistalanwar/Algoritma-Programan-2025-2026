76c9251305f690a73f45662345744ca81c184546
haqsyistalanwar <hhaqsyis@gmail.com>
2025-10-13 09:50:31 +0700

    percobaan-1

---
+++ C++/coba1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+ int a = 24;
+ int b = 4;
+ char c[50] = "Muhammad Abdul Haqsyist Al Anwar";
+
+ puts("Nama ");
+ puts(c);
+ printf("Terdiri dari %d Hurup dan %d Kata ", a, b);
+}
