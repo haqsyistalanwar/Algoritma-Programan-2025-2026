6582c65cc4aabc3feac0a3ed7ec38ffdb15059d6
haqsyistalanwar <hhaqsyis@gmail.com>
2025-11-09 08:53:31 +0700

    variabel data tugas uts

---
+++ JAVA/Variabel.java
@@ -0,0 +1,22 @@
+public class Variabel {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String nama = "Muhammad Abdul Haqsyist Al Anwar";
+        String tempatTanggalLahir = "BOGOR, 14 Mei 2007";
+        String jenisKelamin = "Laki-laki";
+        String alamat = "Jl.Citeureup Tajur TR03/04, BOGOR";
+        String noHp = "089637880467s";
+        double penghasilan = 2500000.0; // bisa bebas sesuai perintah
+
+        // Output
+        System.out.println("Assalamu’alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap         : " + nama);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin        : " + jenisKelamin);
+        System.out.println("Alamat Lengkap       : " + alamat);
+        System.out.println("No HP                : " + noHp);
+        System.out.println("Penghasilan Sebulan  : Rp" + penghasilan);
+    }
+}
